4cd93e0360c63d5351e0c73380cdc20958758195
hiranoxer <seiji.hirano@monoxer.com>
2022-06-07 18:45:15 +0900

    Init HelloWorld App

---
+++ app/src/main/java/com/example/helloworldapp/MainActivity.kt
@@ -0,0 +1,82 @@
+package com.example.helloworldapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import com.example.helloworldapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private var buttonTap: Boolean = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener{
+            if (buttonTap) {
+                binding.textView.text = getString(R.string.hello)
+                buttonTap = false
+            } else {
+                binding.textView.setText(R.string.world)
+                buttonTap = true
+            }
+        }
+    }
+}
+
+/*
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private var textView: TextView? = null
+    private var button: Button? = null
+    private var buttonTap: Boolean = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        textView = findViewById(R.id.text_view)
+        button = findViewById(R.id.button)
+
+        button?.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        if (buttonTap) {
+            textView?.setText(R.string.hello)
+            buttonTap = false
+        } else {
+            textView?.setText(R.string.world)
+            buttonTap = true
+        }
+    }
+}
+*/
+
+//class MainActivity : AppCompatActivity() {
+//    private var textView: TextView? = null
+//    private var button: Button? = null
+//    private var buttonTap: Boolean = false
+//
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_main)
+//
+//        textView = findViewById(R.id.text_view)
+//        button = findViewById(R.id.button)
+//
+//        button?.setOnClickListener {
+//            if (buttonTap) {
+//                textView?.setText("Hello")
+//                buttonTap = false
+//            } else {
+//                textView?.setText("World")
+//                buttonTap = true
+//            }
+//        }
+//    }
+//}
+
