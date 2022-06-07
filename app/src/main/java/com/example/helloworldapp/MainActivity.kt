e591dc42442d33d1fdadefeee5a947120ab0d810
hiranoxer <seiji.hirano@monoxer.com>
2022-06-07 20:16:37 +0900

    Update Layout without xml

--- app/src/main/java/com/example/helloworldapp/MainActivity.kt
+++ app/src/main/java/com/example/helloworldapp/MainActivity.kt
@@ -1,84 +1,61 @@
 package com.example.helloworldapp
 
+import android.app.ActionBar
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Layout
+import android.view.Gravity
 import android.view.View
+import android.view.ViewGroup
 import android.widget.Button
+import android.widget.LinearLayout.*
+import android.widget.LinearLayout
 import android.widget.TextView
 import com.example.helloworldapp.databinding.ActivityMainBinding
+import org.w3c.dom.Text
 
-class MainActivity : AppCompatActivity(), View.OnClickListener {
-    private lateinit var binding: ActivityMainBinding
+class MainActivity : AppCompatActivity() {
     private var buttonTap: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        var dp = resources.displayMetrics.density
+        var sp = resources.displayMetrics.scaledDensity
+        var layout = LinearLayout(this)
 
-        binding.button.setOnClickListener(this)
-    }
+        var mParent = LayoutParams.MATCH_PARENT
+        var wContent = LayoutParams.WRAP_CONTENT
 
-    override fun onClick(v: View) {
-        if (buttonTap) {
-            binding.textView.text = getString(R.string.hello)
-            buttonTap = false
-        } else {
-            binding.textView.setText(R.string.world)
-            buttonTap = true
+        layout.layoutParams = LayoutParams(mParent, mParent)
+        layout.orientation = VERTICAL
+        layout.gravity = Gravity.CENTER
+        layout.setBackgroundColor(Color.rgb(0xdf, 0xff, 0xef))
+
+        setContentView(layout)
+
+        val textView = TextView(this)
+        textView.text = getString(R.string.hello)
+        textView.textSize = 50 * sp
+        textView.layoutParams = LayoutParams(wContent, wContent)
+        layout.addView(textView)
+
+        val button = Button(this)
+        button.text = getString(R.string.button)
+        val buttonLayoutParams = LayoutParams((250 * dp).toInt(), wContent)
+        buttonLayoutParams.topMargin = (40 * dp).toInt()
+        button.layoutParams = buttonLayoutParams
+
+        layout.addView(button)
+
+        button.setOnClickListener{
+            if (buttonTap) {
+                textView.text = getString(R.string.hello)
+                buttonTap = false
+            } else {
+                textView.setText(R.string.world)
+                buttonTap = true
+            }
         }
     }
 }
-
-/*
-class MainActivity : AppCompatActivity(), View.OnClickListener {
-    private var textView: TextView? = null
-    private var button: Button? = null
-    private var buttonTap: Boolean = false
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        textView = findViewById(R.id.text_view)
-        button = findViewById(R.id.button)
-
-        button?.setOnClickListener(this)
-    }
-
-    override fun onClick(v: View) {
-        if (buttonTap) {
-            textView?.setText(R.string.hello)
-            buttonTap = false
-        } else {
-            textView?.setText(R.string.world)
-            buttonTap = true
-        }
-    }
-}
-*/
-
-//class MainActivity : AppCompatActivity() {
-//    private var textView: TextView? = null
-//    private var button: Button? = null
-//    private var buttonTap: Boolean = false
-//
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_main)
-//
-//        textView = findViewById(R.id.text_view)
-//        button = findViewById(R.id.button)
-//
-//        button?.setOnClickListener {
-//            if (buttonTap) {
-//                textView?.setText("Hello")
-//                buttonTap = false
-//            } else {
-//                textView?.setText("World")
-//                buttonTap = true
-//            }
-//        }
-//    }
-//}
-
